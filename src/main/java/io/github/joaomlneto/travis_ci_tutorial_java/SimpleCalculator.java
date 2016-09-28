28485aa878c9a1a7b46a4a7b7fac8a5e0e07ec75
cevenhub <cevenmail@gmail.com>
2016-09-28 11:52:01 -0700

    Test Changes
    Newlines were added

--- src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
@@ -4,15 +4,19 @@ public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	
 	public int sub(int a, int b) {
 		return a - b;
 	}
+	
 	public int mul(int a, int b) {
 		return a * b;
 	}
+	
 	public int div(int a, int b) {
 		return a / b;
 	}
+	
 	public static final void main(String[] args) {
 		System.out.println("Hello world?");
 	}
